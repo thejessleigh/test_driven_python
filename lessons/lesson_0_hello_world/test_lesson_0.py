d789bdd5e018e12e8d77b0ac845acbdacc7af1b2
thejessleigh <j.l.unrein@gmail.com>
2019-03-15 23:19:17 -0500

    Slight rewrites of lessons 0 and 1 for Python3

---
+++ lessons/lesson_0_hello_world/test_lesson_0.py
@@ -0,0 +1,26 @@
+# This lesson teaches you how to define a Python method. Try running the tests for this file before doing anything!
+
+# To run the file, navigate to the lesson 0 directory in your terminal and type `python test_lesson_0.py`.
+# That command looks like a lot at first. Let's break it down.
+
+# What error did you get when you first ran it?
+# Remember, errors are built to be **helpful**, not scary!
+
+# It should be an ImportError. This is Python telling you that it's unable to import the file `hello_world`
+
+# So your first step should be to create `hello_world.py` in the `lessons/lesson_0_hello_world` directory
+
+# Run the tests again after each code change. Repeat this process until the test file runs without outputting anything.
+
+import pytest
+
+from hello_world import hello_world
+
+
+def test_hello_function_exists():
+    func = hello_world.hello_world
+    assert func is not None
+
+
+def test_hello_output():
+    assert hello_world.hello_world() == "Hello World!"
---
+++ lessons/lesson_1_greetings/test_lesson_1.py
@@ -0,0 +1,31 @@
+# This lesson teaches you how to define a function that takes an input, and then uses
+# it to alter the output of that function.
+
+# Much like last time, navigate to the lesson_1_greetings directory in your terminal and run your python command
+# `python test_lesson_1.py`
+
+# Again, like last time, you should see an import error indicating that you need a file titled
+# `greetings` in your lesson folder.
+
+# This challenge is very similar to the last one, but instead of always outputting the same string - "Hello World!",
+# the output will change based on the input you give to the function.
+
+
+import pytest
+
+import greetings
+
+
+def test_greetings_function_exists():
+    func = greetings.greet
+    assert func is not None
+
+
+def test_greetings_function_with_input():
+    greet = greetings.greet("Anna")
+    assert greet == "Hi, Anna!"
+
+
+def test_greetings_function_with_another_input():
+    greet = greetings.greet("Elsa")
+    assert greet == "Hi, Elsa!"
